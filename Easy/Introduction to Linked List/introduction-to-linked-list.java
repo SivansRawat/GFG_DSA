c7d9f809c3c5f088d76c1cf814b15cc6fd47a036
SivansRawat <122634172+SivansRawat@users.noreply.github.com>
2023-03-18 18:58:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Introduction to Linked List/introduction-to-linked-list.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            Node ans = obj.constructLL(a);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        
+        Node head = null;
+        Node tail = null;
+        
+        for(int i =0;i<arr.length;i++){
+            
+            
+            if(head == null){
+                head = new Node(arr[i]);
+                tail = head ;
+            }
+            
+            else{
+                
+                tail.next = new Node(arr[i]);
+                tail = tail.next;
+            }
+        }
+        
+        
+        return head;
+        
+        
+        
+        
+    }
+}
