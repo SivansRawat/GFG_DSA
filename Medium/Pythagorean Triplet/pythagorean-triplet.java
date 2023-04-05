601f6f51545a1cbb25555dfc5112a4e297b88b20
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-05 18:29:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Pythagorean Triplet/pythagorean-triplet.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        
+        // code here
+        
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    // Calculate square of array elements
+                    int x = arr[i] * arr[i], y = arr[j] * arr[j], z = arr[k] * arr[k];
+ 
+                    if (x == y + z || y == x + z || z == x + y)
+                        return true;
+                }
+            }
+        }
+ 
+        // If we reach here, no triplet found
+        return false;
+    }
+}
