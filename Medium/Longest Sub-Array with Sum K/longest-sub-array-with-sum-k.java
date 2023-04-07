397c8a5c78c2c12411073c194e1c11d4d196c502
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-07 21:03:02 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        
+        int curSum =0;
+        int start = 0;
+        int end = -1;
+        int max =0; 
+        
+        
+        // Solution using HashMap  but this question is of Variable Sliding Window 
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        
+        for(int i =0;i<N;i++){
+            
+            curSum += A[i];
+            
+            
+            if((curSum - K) ==0 ){
+                max = Math.max(max,i+1);
+                
+            
+            }
+            
+            
+            if(map.containsKey(curSum-K)){
+                
+                max = Math.max(max,i-map.get(curSum-K));
+    
+            }
+            
+            if(!map.containsKey(curSum)){
+                map.put(curSum,i);
+            }
+            
+            
+            
+        }
+        
+        
+        return max;
+        
+    }
+    
+    
+}
+
+
