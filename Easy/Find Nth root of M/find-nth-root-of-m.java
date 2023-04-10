416b6fc0cc76a326f9c14f77e1a7d24260830e6e
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-10 17:29:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Nth root of M/find-nth-root-of-m.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            String[] S1 = s.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            Solution ob = new Solution();
+            int ans = ob.NthRoot(n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int NthRoot(int n, int m)
+    {
+        // code here
+        
+        int start = 1;
+        int end = m;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(Math.pow(mid,n) == m) return mid;
+            else if(Math.pow(mid,n) > m) end = mid-1;
+            else start = mid+1;
+        }
+         return -1;
+    }
+}
