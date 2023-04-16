85dad5bccfc089a3f5f765c9569a5afc097a5439
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-16 15:43:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Subarray with given sum/subarray-with-given-sum.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        // // int []  res = new int[2];
+        // ArrayList<Integer> res = new ArrayList<>();
+        
+        // Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        // for(int i = 0 ; i<arr.length;i++){
+
+        //     if(map.containsKey(s - arr[i])){
+               
+        //         res.add(map.get(s - arr[i]) +1) ;
+        //         res.add(i +1) ;
+        //         return res ;
+        //     }
+
+        //     map.put(arr[i] , i);
+        
+        // }
+
+        // return res ;
+        
+        
+               ArrayList<Integer> a = new ArrayList<Integer>();
+        if(s==0){
+            a.add(-1);
+            return a;
+        }
+        int i=0,j=0;
+        int sum = arr[0];
+        while(j<n){
+            if(sum == s){
+                
+                a.add(i+1);
+                a.add(j+1);
+                return a;
+            }
+            else if(sum<s){
+                j++;
+                if(j<n){
+                    sum += arr[j];
+                }
+            }else{
+                sum -= arr[i];
+                i++;
+            }
+        }
+        
+        a.add(-1);
+        return a;
+    }
+}
