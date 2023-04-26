ebe3cda4e50e4b940b955123bb421f4d253ba97d
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-04-26 18:35:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Parenthesis Checker/parenthesis-checker.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+        Stack<Character>st = new Stack<>();
+        for(int i=0;i<x.length();i++){
+            char ch = x.charAt(i);
+            if(st.isEmpty()){
+                st.push(ch);
+            }
+            else if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else if(ch==')' && st.peek()=='('){
+                st.pop();
+            }
+            else if(ch=='}' && st.peek()=='{'){
+                st.pop();
+            }
+            else if(ch==']' && st.peek()=='['){
+                st.pop();
+            }
+            else {
+                st.push(ch);
+            }
+        }
+        return st.isEmpty();
+        
+
+        
+    }
+    
+}
