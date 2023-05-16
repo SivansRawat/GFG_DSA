7c4d5f08cd39f4e9eddc69141a240327fb9ddf27
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-05-16 20:03:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Total Setbits/count-total-setbits.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countBits(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int countBits(int N){
+        // code here
+        int sum =0;
+        for(int i=1;i<=N;i++){
+            sum+=Integer.bitCount(i);
+        }
+        
+        return sum;
+    }
+}
