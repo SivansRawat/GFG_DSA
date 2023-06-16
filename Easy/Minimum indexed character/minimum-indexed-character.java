9c538546b324173d9814f2da3b7904665101828d
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-06-16 19:41:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum indexed character/minimum-indexed-character.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find the minimum indexed character.
+    public static int minIndexChar(String str, String patt)
+    {
+        // Your code here
+        
+        String s="";
+
+        for(int i=0;i<str.length();i++){
+
+            s+=str.charAt(i);
+
+            if(patt.contains(s)) return i;
+
+            s="";
+
+        }
+
+        return -1;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t>0)
+		{
+		    t--;
+		    
+		    String s1=sc.next();
+		    String s2=sc.next();
+		    
+		    int res = new Solution().minIndexChar(s1, s2);
+		    System.out.println(res);
+		}
+	}
+}
+
+// } Driver Code Ends
