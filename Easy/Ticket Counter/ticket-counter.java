51e7d379eb86d53ca1e79946596a541b389c9996
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-06-18 17:51:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Ticket Counter/ticket-counter.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s[]=in.readLine().trim().split(" ");
+            int n=Integer.parseInt(s[0]);
+            int k=Integer.parseInt(s[1]);
+            Solution ob=new Solution();
+            out.println(ob.distributeTicket(n,k));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public static int distributeTicket(int N,int K)
+    {
+        
+        
+        int i = 1, j = N;
+        Stack<Integer> st = new Stack<>();
+        int a = 1;
+        while (i < j) {
+            for (int x = 0; x < K; x++) {
+                st.push(a++);
+                i++;
+                if (i == j)
+                    break;
+            }
+            for (int y = 0; y < K; y++) {
+                st.push(N--);
+                j--;
+                if (i == j)
+                    break;
+            }
+        }
+        return i;
+        
+    }
+}
