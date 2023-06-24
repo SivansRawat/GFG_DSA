22af8bf18aad9941ff9a0dfc3546390130467c40
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-06-24 14:08:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix match with other strings/prefix-match-with-other-strings.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String[] arr = new String[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = sc.next();
+            }
+            
+            int k = Integer.parseInt(sc.next());
+            
+            String str = sc.next();
+            
+            Solution obj = new Solution();
+            int ans = obj.klengthpref(arr,n,k,str);
+            System.out.println(ans);
+            
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int klengthpref(String[] arr, int n, int k, String str)
+    {
+     
+     
+        // code here
+        
+        
+         int x=0;
+        for(int i=0;i<n;i++){
+            if(arr[i].length()<k || str.length()<k){
+                x=x+0;
+            }
+            else{
+                if(arr[i].substring(0,k).equals(str.substring(0,k))){
+                x++;
+            }
+            }
+            
+        }
+        return x;
+    }
+}
