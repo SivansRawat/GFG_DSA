e502ab81e49bf82703da13d24a3376c3c8044079
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-07-04 20:24:54 +0530

    Updated Solution - GfG to GitHub

--- Easy/Array Subset of another array/array-subset-of-another-array.java
+++ Easy/Array Subset of another array/array-subset-of-another-array.java
@@ -50,17 +50,27 @@ class Compute {
         
         
         ArrayList list=new ArrayList<>();
-for(int i=0;i<n;i++){
-list.add(a1[i]);
-}
-for(int i=0;i<m;i++){
-if(list.contains(a2[i])){
-list.remove(a2[i]);
-}else{
-return "No";
-}
-}
-return "Yes";
+        for(int i=0;i<n;i++){
+            list.add(a1[i]);
+        }
+        
+        
+        for(int i=0;i<m;i++){
+            if(list.contains(a2[i])){
+                list.remove(a2[i]);
+                
+            }
+            
+            else{
+                
+                return "No";
+                
+            }
+            
+        }
+        
+        
+        return "Yes";
         
     }
 }
