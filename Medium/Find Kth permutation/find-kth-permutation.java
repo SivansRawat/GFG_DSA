e7434c4465b1ede3952c6ff9d8eab138836dfadc
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-07-12 11:21:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Kth permutation/find-kth-permutation.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+// } Driver Code Ends
+class Solution {
+    public static String kthPermutation(int n,int k) {
+        // code here
+        
+         int fact = 1;
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int i=1;i<n;i++){
+            fact = fact*i;
+            arr.add(i);
+            
+        }
+        
+        arr.add(n);
+        
+        String ans = "";
+        
+        k = k-1;
+        
+        while(true){  // Infinite loop till list is zero
+            
+            ans = ans + arr.get(k/fact);
+            arr.remove(k/fact);
+            
+            if(arr.size() ==0 ){
+                break;
+            }
+            
+            
+            k = k%fact;
+            fact = fact/arr.size();
+            
+        }
+        
+        return ans;
+    }
+}
+        
+
+
+//{ Driver Code Starts.
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int[] a = IntArray.input(br, 2);
+            
+            Solution obj = new Solution();
+            String res = obj.kthPermutation(a[0],a[1]);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
