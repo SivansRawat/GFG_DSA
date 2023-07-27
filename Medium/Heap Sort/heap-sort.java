793611d6c8f10a9c96ac0f3b8817b158e3fb3a8f
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-07-27 13:30:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Heap Sort/heap-sort.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+class Heap_Sort
+{
+    void printArray(int arr[],int n)
+    {
+        //int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc  = new Scanner(System.in);
+        Heap_Sort hs = new Heap_Sort();
+        int arr[] = new int[1000000];
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int n = sc.nextInt();
+            for(int i=0;i<n;i++)
+                arr[i] = sc.nextInt();
+                
+            Solution ob=new Solution();
+            ob.heapSort(arr,n);
+            hs.printArray(arr,n);
+            T--;
+        }
+    }
+    
+}
+
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    // Function to build a Heap from the array.
+    void buildHeap(int arr[], int n)
+    {
+        // Starting from the first non-leaf node and going up to the root,
+        // perform heapify to create a max heap.
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    // Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as the root
+        int leftChild = 2 * i + 1; // Left child of the current node
+        int rightChild = 2 * i + 2; // Right child of the current node
+
+        // If the left child is larger than the root
+        if (leftChild < n && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        // If the right child is larger than the root
+        if (rightChild < n && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        // If the largest is not the root
+        if (largest != i) {
+            // Swap the largest element with the root
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected subtree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        // Build the max heap from the given array.
+        buildHeap(arr, n);
+
+        // Extract elements from the heap one by one
+        // and put them at the end of the array.
+        for (int i = n - 1; i > 0; i--) {
+            // Move the current root to the end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+}
