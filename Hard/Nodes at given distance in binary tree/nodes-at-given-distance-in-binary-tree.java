7d6562df1d65d5cd01c70ef8a34c815c7ad42653
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-08-22 20:23:53 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.java
@@ -0,0 +1,146 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException
+    {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine().trim();
+            Node root = buildTree(s);
+            Solution T = new Solution();
+            int target = Integer.parseInt(br.readLine().trim());
+            int k = Integer.parseInt(br.readLine().trim());
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = T.KDistanceNodes(root,target,k);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();    
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//     int data; 
+//     Node left, right;
+// }
+
+class Solution{
+     Map<Node, Node> par = new HashMap<>();
+    Map<Node, Integer> vis = new HashMap<>();
+    ArrayList<Integer> ans = new ArrayList<>();
+    Node t;
+
+    public void parent(Node root, int target) {
+        if (root.data == target) t = root;
+        if (root.left != null) {
+            par.put(root.left, root);
+            parent(root.left, target);
+        }
+        if (root.right != null) {
+            par.put(root.right, root);
+            parent(root.right, target);
+        }
+    }
+
+    public void dfs(Node node, int k) {
+        if (node == null || vis.containsKey(node)) return;
+        vis.put(node, 1);
+        if (k == 0) {
+            ans.add(node.data);
+            return;
+        }
+        dfs(node.left, k - 1);
+        dfs(node.right, k - 1);
+        dfs(par.get(node), k - 1);
+    }
+
+    public ArrayList<Integer> KDistanceNodes(Node root, int target, int k) {
+        ans.clear();
+        parent(root, target);
+        dfs(t, k);
+        Collections.sort(ans);
+        return ans;
+    }
+};
