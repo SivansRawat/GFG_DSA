dc96ad52720830f65b1465fda2bcb989b061eec0
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2023-11-21 19:35:37 +0530

    Updated Solution - GfG to GitHub

--- Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.java
+++ Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.java
@@ -135,7 +135,7 @@ class Solution
 	{
 	    // Code Here
 	    
-	    if(root1 == null || root2 == null) return (root1==root2);
+	      if(root1 == null || root2 == null) return (root1==root2);
 	    
 	    return (root1.data == root2.data) && isIdentical(root1.left,root2.left) && isIdentical(root1.right,root2.right);
 	}
