a416979230e6e6d1e3cf49bd7ce936541fcc0055
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2024-01-03 22:32:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.java
+++ Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -27,26 +27,47 @@ class GFG {
 class Solution
 {
     //Function to find the length of longest common subsequence in two strings.
+    
+    
     static int lcs(int x, int y, String s1, String s2)
     {
         // your code here
         
+        int[][] dp = new int[x+1][y+1];
         
-        int arr[][] = new int[x+1][y+1];
-        for(int i=x; i>=0;i--){
-            for(int j=y; j>=0; j--){
-                if(i==x|| j==y){
-                    arr[i][j]=0;
-                }
-                else if(s1.charAt(i)==s2.charAt(j)){
-                    arr[i][j]= 1 + arr[i+1][j+1];
-                }
-                else if(s1.charAt(i)!=s2.charAt(j)){
-                    arr[i][j] = Math.max(arr[i][j+1],arr[i+1][j]);
-                }
+       
+        
+        for(int i=0 ;i<x+1;i++){
+            for(int j=0;j<y+1;j++){
+                if(i==0 || j==0) dp[i][j] =  dp[i][j] = 0;
             }
         }
-        return arr[0][0];
+        
+    
+        for(int i=1;i<x+1;i++){
+            for(int j=1;j<y+1;j++) {
+        
+
+        
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
+            
+           
+            dp[i][j] =  1+dp[i-1][j-1];
+        }
+        
+        
+        else{
+            
+            dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+        }
+        
+        
+            }
+        }
+        
+        
+        return dp[x][y];
+        
     }
     
 }
