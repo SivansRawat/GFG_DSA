c6cb683cf6fa158caf2847e143494a5c74598ed6
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2024-01-13 20:34:29 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number Of Enclaves/number-of-enclaves.java
+++ Medium/Number Of Enclaves/number-of-enclaves.java
@@ -36,56 +36,69 @@ class GFG {
 
 // User function Template for Java
 
+class Pair {
+    int first;
+    int second; 
+    public Pair(int first, int second) {
+        this.first = first; 
+        this.second = second; 
+    }
+
+}
 class Solution {
-     int temprow[]={-1,0,1,0};
-     int tempcol[]={0,1,0,-1};
-     public void dfs(int grid[][],int vis[][],int row,int col)
-     {
-         vis[row][col]=1;
-         int m=grid.length;
-         int n=grid[0].length;
-         
-         for(int i=0;i<4;i++)
-         {
-             int nrow=temprow[i]+row;
-             int ncol=tempcol[i]+col;
-             
-             if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && vis[nrow][ncol]!=1)
-             {
-                 dfs(grid,vis,nrow,ncol);
-             }
-         }
-     }
+
     int numberOfEnclaves(int[][] grid) {
 
         // Your code here
-        int m=grid.length;
-        int n=grid[0].length;
-        int vis[][]=new int[m][n];
         
-        for(int i=0;i<n;i++)
-        {
-            if(grid[0][i]==1 && vis[0][i]!=1)
-            dfs(grid,vis,0,i);
-            if(grid[m-1][i]==1 && vis[m-1][i]!=-1)
-            dfs(grid,vis,m-1,i);
-        }
-        for(int i=0;i<m;i++)
-        {
-            if(grid[i][0]==1 && vis[i][0]!=1)
-            dfs(grid,vis,i,0);   
-            if(grid[i][n-1]==1 && vis[i][n-1]!=1)
-            dfs(grid,vis,i,n-1);
-        }
-        int ans=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==1 && vis[i][j]==0)
-                ans++;
+        
+        Queue<Pair> q = new LinkedList<Pair>();
+        int n = grid.length; 
+        int m = grid[0].length; 
+        int vis[][] = new int[n][m];
+        // traverse boundary elements
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // first row, first col, last row, last col 
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    // if it is a land then store it in queue
+                    if(grid[i][j] == 1) {
+                        q.add(new Pair(i, j)); 
+                        vis[i][j] = 1; 
+                    }
+                }
             }
         }
-        return ans;
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.isEmpty()) {
+            int row = q.peek().first; 
+            int col = q.peek().second; 
+            q.remove(); 
+            
+            // traverses all 4 directions
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+                // check for valid coordinates and for land cell
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.add(new Pair(nrow, ncol));
+                    vis[nrow][ncol] = 1; 
+                }
+            }
+            
+        }
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // check for unvisited land cell
+                if(grid[i][j] == 1 & vis[i][j] == 0) 
+                    cnt++; 
+            }
+        }
+        return cnt; 
     }
 }
