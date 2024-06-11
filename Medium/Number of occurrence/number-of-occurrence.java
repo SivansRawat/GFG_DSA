797965fc1b2c58c94d63eb5e242df0fc494cd0c2
Sivans Rawat <122634172+SivansRawat@users.noreply.github.com>
2024-06-11 16:44:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.java
+++ Medium/Number of occurrence/number-of-occurrence.java
@@ -38,13 +38,64 @@ public class Main {
 class Solution {
     int count(int[] arr, int n, int x) {
         // code here
+        int[] ans = firstAndLastPosition(arr, n, x);
+        if (ans[0] == -1) return 0;
+        return (ans[1] - ans[0] + 1);
+     
         
-        int count=0;
-        for(int i=0; i<n; i++){
-            if(arr[i]==x){
-                count++;
+    }
+    
+     public static int[] firstAndLastPosition(int[] arr, int n, int k) {
+        int first = firstOccurrence(arr, n, k);
+        if (first == -1) return new int[] { -1, -1};
+        int last = lastOccurrence(arr, n, k);
+        return new int[] {first, last};
+    }
+    
+    
+    
+     public static int firstOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int first = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (arr[mid] == k) {
+                first = mid;
+                
+                high = mid - 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
             }
         }
-        return count;
+        return first;
     }
+
+    public static int lastOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == k) {
+                last = mid;
+               
+                low = mid + 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return last;
+    }
+    
+     
+    
+    
+    
 }
